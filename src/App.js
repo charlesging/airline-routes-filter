addfdba158d7891049c97fd1bc7bea2363eb8541
Charles Ging <charlesging@Charless-iMac.lan1>
2019-09-04 16:38:04 -0500

    display raw data in App component table element

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
+import Data from './data.js'
 import './App.css';
 
 class App extends Component {
+
   render() {
+    const routes = Data.routes.map((route) => {
+      return <tr>
+        <td>{ route.airline }</td>
+        <td>{ route.src }</td>
+        <td>{ route.dest }</td>
+      </tr>
+    });
+
     return (
       <div className="app">
         <header className="header">
           <h1 className="title">Airline Routes</h1>
         </header>
         <section>
-          <p>
-            Welcome to the app!
-          </p>
+          <table class="routes-table">
+            <thead>
+            </thead>
+            <tbody>
+              { routes }
+            </tbody>
+          </table>
         </section>
       </div>
     );
